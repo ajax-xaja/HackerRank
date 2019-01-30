6951e07f2480e7b3c398d1e2adf23364a2c3d91c
ajax-xaja <kevinchristophercruz@gmail.com>
2019-01-29 16:28:43 -0800

    Adds Strings problems 2 and 3

---
+++ Java/Strings/03 - Java Substring Comparisons/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        for(int i = 0; i <= s.length() - k; i++) {
+            String substring = s.substring(i, (i + k));
+
+            if(substring.compareTo(smallest) < 0) {
+                smallest = substring;
+            }
+
+            if(substring.compareTo(largest) > 0) {
+                largest = substring;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static String getSmallestAndLargestImproved(String s, int k) {
+        SortedSet<String> sets = new TreeSet<String>();
+
+        for(int i = 0; i <= s.length() - k; i++) {
+            sets.add(s.substring(i, i + k));
+        }
+
+        return sets.first() + "\n" + sets.last();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
